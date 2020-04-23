9cceec384e6831235711ee2e645c9d2ffb4bb855
QL805 <805008123@qq.com>
2020-04-23 15:02:33 +0800

    Android2

--- app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180212/Ch10Activity2.java
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180212/Ch10Activity2.java
@@ -1,9 +1,13 @@
 package cn.edu.sdwu.android02.classroom.sn170507180212;
 
 import android.content.Intent;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Ch10Activity2 extends AppCompatActivity {
 
@@ -12,11 +16,85 @@ public class Ch10Activity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_ch10_2);
     }
+
     public void send_broadcast(View view){
         //发送广播
         Intent intent=new Intent("com.inspur.broadcast");
         intent.putExtra("key1","message");
 
         sendBroadcast(intent);//发送
+    }
+    public void ch10Activity(View view){
+        Intent intent=new Intent(this,Ch10Activity1.class);
+        EditText editText= (EditText) findViewById(R.id.ch10_2_et);
+        intent.putExtra("text",editText.getText().toString());
+        startActivity(intent);
+    }
+
+    public void startSubActivity(View view){
+        //1.以Sub_activity的方式启动子Activity
+        Intent intent=new Intent(this,Ch10Activity3.class);
+        startActivityForResult(intent,101);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //3.在父Activity中获取的返回值
+        //requestCode用来区分哪一个activity返回的结果
+        if (requestCode==101){
+            if (requestCode==RESULT_OK){
+                //用户点击确认，进一步获取数据
+                String name=data.getStringExtra("name");
+                Toast.makeText(this,name,Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this,"Cancel",Toast.LENGTH_SHORT).show();
+            }
+
+        }else if(requestCode==102){
+            if (requestCode==RESULT_OK){
+                //得到联系人信息（联系人的编号）
+                String content=data.getDataString();
+                Toast.makeText(this,content,Toast.LENGTH_SHORT).show();
+            }else{
+                Toast.makeText(this,"Cancel",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    public void web(View view){
+        //使用隐式启动方式，打开网页
+        Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse("http://baidu.com"));
+        startActivity(intent);
+    }
+    public void contactsList(View view){
+        Intent intent=new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("content://contacts/people/"));
+        startActivity(intent);
+    }
+    public void contactsDetail(View view){
+        //查看联系人明细
+        Intent intent=new Intent(Intent.ACTION_EDIT);
+        intent.setData(Uri.parse("content://contacts/people/1"));
+        startActivity(intent);
+    }
+
+    public void showMap(View view){
+        Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("geo:50.123,7.1234"));
+        startActivity(intent);
+    }
+
+    public void showPhoto(View view){
+        Intent intent=new Intent(Intent.ACTION_VIEW,Uri.parse("\"content://media/external/images/media/\""));
+        startActivity(intent);
+    }
+    public void pickContact(View view){
+        //以子Activity的形式，打开联系人列表，用户选择一个后，返回结果
+        Intent intent=new Intent(Intent.ACTION_PICK);//隐式启动
+        intent.setData(ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent,102);
+    }
+    public void implicitStart(View view){
+        Intent intent=new Intent(("com.inspur.action2"));
+        intent.setData(Uri.parse("abc://inspur.com"));
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/cn/edu/sdwu/android02/classroom/sn170507180212/Ch10Activity3.java
@@ -0,0 +1,33 @@
+package cn.edu.sdwu.android02.classroom.sn170507180212;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class Ch10Activity3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_ch10_3);
+    }
+
+    public void ok(View view){
+        //2.设置子Activity的返回值
+        EditText editText= (EditText) findViewById(R.id.ch10_3_et);
+        String content=editText.getText().toString();
+        //
+        Intent intent=new Intent();
+        intent.putExtra("name",content);
+        setResult(RESULT_OK,intent);//设置返回值
+
+        finish();//关闭当前界面
+    }
+
+    private void cancel(View view) {
+        setResult(RESULT_CANCELED);//取消
+        finish();//关闭当前界面
+    }
+}
